294ce20c3c13a5af899147f0ebe1feb0279bd18f
Yehiahany2005 <yehia.shorim@gmail.com>
2025-05-08 23:49:54 +0300

    Modification to Landing Page: - Changed all the hero section (Section 1) - Improved css for section 1 and 2 - Made the page fully responsive on mobiles - Fixed Footer

--- JavaScript/UniBite.js
+++ JavaScript/UniBite.js
@@ -3,33 +3,40 @@ document.addEventListener("DOMContentLoaded", () => {
         // Laptop Animation
         ScrollTrigger.create({
             animation: gsap.fromTo(".Main-logo", {
-                y: "40vh", //Start position
+                y: "20vh", //Start position
                 scale: 1.3, //Start Scale
             }, {
-                y: "-2vh", //ENd location
-                scale: 0.5, //End Scale / size
+                y: "0vh", //ENd location
+                scale: 0.6, //End Scale / size
                 ease: "power2.out",
             }),
             scrub: true,
             trigger: ".Section2",
             start: "top bottom",
-            end: "top center",
+            end: "top bottom",
         });
     } else {
         // Mobile Animation
         ScrollTrigger.create({
             animation: gsap.fromTo(".Main-logo", {
-                y: "40vh",
+                y: "20vh",
                 scale: 2.5,
             }, {
-                y: "2vh",
-                scale: 1,
+                y: "2.5vh",
+                scale: 1.5,
                 ease: "power2.out",
             }),
             scrub: true,
             trigger: ".Section2",
             start: "top bottom",
-            end: "top center",
+            end: "top bottom",
         });
     }
+
+
+
+
 });
+
+
+
