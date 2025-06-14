b22cb786704c7bb43d5a281680f5068902b8b98e
Prince <belalelprince22@gmail.com>
2025-06-14 05:59:07 +0300

    Some UI modifications

--- server.js
+++ server.js
@@ -192,5 +192,6 @@ app.use((req, res) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000
+000;
 server.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
